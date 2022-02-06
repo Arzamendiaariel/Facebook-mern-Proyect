611ed3fea7da6794c5225bf0351d0dcdcd5edd11
arzamendiaariel <arzamendiaariel@gmail.com>
2022-02-06 01:22:29 -0300

    First steps

---
+++ fb-mern/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Header from './Components/Header';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      
+      <Header />
+      {/* sidebar */}
+      {/* feed */}
+      {/* widgets */}
+    </div>
+  );
+}
+
+export default App;
